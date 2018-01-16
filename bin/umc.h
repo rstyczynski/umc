f6a28a1491b986916cd8d010c7102952f46f8f73
rstyczynski <rstyczynski@HelloBeHealthy.local>
2018-01-16 19:23:00 +0100

    .

--- bin/umc.h
+++ bin/umc.h
@@ -2,6 +2,8 @@
 #--- Library
 #---
 
+umc_version=0.1
+
 if [ -z "$umcRoot" ]; then
   export umcRoot="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 fi
@@ -9,7 +11,7 @@ fi
 export toolsBin=$umcRoot/bin
 
 # configure
-. $umcRoot/etc/config.h
+. $umcRoot/etc/umcConfig.h
 
 # ATTENTION!
 # Do not use buffered!
@@ -17,36 +19,93 @@ export toolsBin=$umcRoot/bin
 # It will be moment of buffer flush not a proper moment of collecting data.
 export BUFFERED=no
 
-function decodeVersion {
-  version_full=$(uname -r | cut -d'-' -f1)
-  version_specific=$(uname -r | cut -d'-' -f2-999)
 
-  version_major=$(uname -r | cut -d'-' -f1 | cut -d'.' -f1)
-  version_minor=$(uname -r | cut -d'-' -f1 | cut -d'.' -f2)
-  version_patch=$(uname -r | cut -d'-' -f1 | cut -d'.' -f3)
+function availableSensors {
+    cat $umcRoot/etc/umcConfig.h | grep "_layer=" | cut -f1 -d'=' | cut -f2 -d' ' | cut -f1 -d'_'
 }
 
+function usage {
 
-# list parent layers in platform layer stack
-# for soa produces: Linux java wls
-function getParentLayers {
-  unset layers
-  layer=$1
-  until [ "$layer" = "/" -o "$layer" = "" ]; do
-    layers="$layer/$layers"
-    layer=$(eval "echo $(echo \$$layer\_layer)")
-  done 
-  echo "/$layers" | sed "s/\/$1//"
+    cat <<EOF
+Universal Metrics Collector. Collects system monitoring data and presents in CSV format.
+
+Usage: umc [-V|test|help] [SENSOR collect delay count] 
+
+    Sensor commands:
+        SENSOR.......sensor to collect data
+
+        collect......collect data
+        delay........delay in seconds between data collections
+        count........number of data collections
+
+    General commands:
+        sensors......list available sensors
+        test.........perform simple test
+
+        help.........this help
+        -V...........get version information
+
+Example:
+    umc free collect 5 2
+    datetime,timezone,timestamp,system,source,total,used,free,shared,buffers,cached,usedNoBuffersCache,freePlusBuffersCache,SwapTotal,SwapUsed,SwapFree
+    2018-01-11 05:00:55,-0800,1515675655,soabpm-vm.site,free,5607456,5123548,483908,0,24384,1061536,4037628,1569828,4128764,163332,3965432
+    2018-01-11 05:01:00,-0800,1515675660,soabpm-vm.site,free,5607456,5134916,472540,0,24400,1062808,4047708,1559748,4128764,163284,3965480
+
+EOF
+
 }
 
-# list child layers in platform layer stack
-# for java produces: wls soa
-function getChildLayers {
-  unset layers
-  layer=$1
-  echo $platformLayers | sed "s/^.*$layer\(.*\)/\1/g"
+function version {
+    cat <<EOF
+umc version $umc_version
+ryszard.styczynski@oracle.com, https://github.com/rstyczynski/tools/tree/master/umc
+EOF
 }
 
+# main wrapper over other methods. User should use this command to use umc
+function umc {
+    sensor=$1; shift
+    if [ $sensor = help -o $sensor = -V -o $sensor = test -o $sensor = sensors ]; then
+        command=$sensor
+    else
+        command=sensor_$1; shift
+        delay=$1; shift
+        count=$1; shift
+        params=$@
+    fi
+     
+    case $command in
+        sensor_help)
+            echo TODO help for sensor
+        ;;
+        
+        sensor_collect)
+            invoke $sensor $delay $count $params
+        ;;
+        
+        sensors)
+            echo $(availableSensors)
+        ;;
+        
+        test)
+            umcTestRun
+        ;;
+        
+        -V)
+            version
+        ;;
+        
+        help) 
+            usage
+            return 0
+        ;;
+        
+        *)
+            usage
+            return 1
+        ;;
+    esac
+}
 
 function getLayerDirectories {
   local layer=$1
@@ -56,24 +115,23 @@ function getLayerDirectories {
   layer_version_patch=$(eval "echo $(echo \$$layer\_version_patch)")
   layer_version_specific=$(eval "echo $(echo \$$layer\_version_specific)")
   
-  if [ ! "$layer/$layer_version_major/$layer_version_minor/$layer_version_patch/$layer_version_specific" = "hardware////" ]; then
+  if [ ! "$layer/$layer_version_major/$layer_version_minor/$layer_version_patch/$layer_version_specific" = "$layer////" ]; then
     echo "$layer/$layer_version_major/$layer_version_minor/$layer_version_patch/$layer_version_specific"
   fi
-  if [ ! "$layer/$layer_version_major/$layer_version_minor/$layer_version_patch" = "hardware///" ]; then
+  if [ ! "$layer/$layer_version_major/$layer_version_minor/$layer_version_patch" = "$layer///" ]; then
     echo "$layer/$layer_version_major/$layer_version_minor/$layer_version_patch"
   fi
-  if [ ! "$layer/$layer_version_major/$layer_version_minor" = "hardware//" ]; then
+  if [ ! "$layer/$layer_version_major/$layer_version_minor" = "$layer//" ]; then
     echo "$layer/$layer_version_major/$layer_version_minor"
   fi
-  if [ ! "$layer/$layer_version_major" = "hardware/" ]; then
+  if [ ! "$layer/$layer_version_major" = "$layer/" ]; then
     echo "$layer/$layer_version_major"
   fi
   echo "$layer"
   
 }
 
-
-getDirectories() {
+function getDirectories {
   layer=$1
   
   for directoryRoot in ""; do
@@ -128,6 +186,11 @@ function locateToolExecDir {
   
   if [ -z $toolExecDir ]; then
     echo "Error! Reason: utility not recognized as supported tool."
+    echo "Available versions:"
+    find $umcRoot/tools -name $cmd -type f | sed 's/^/--- /g'
+    echo "Your version:"
+    cmd_version=$(eval "echo $(echo \$$cmd\_package)")
+    echo "--- $cmd_version"
     return 3
   fi
 }
@@ -213,9 +276,6 @@ function invoke {
   #setBuffered or not buffered operation
   cfgBuffered
 
-  #decode current system version 
-  decodeVersion
-
   #locate tool definition directory
   locateToolExecDir $cmd
   if [ $? -eq 3 ]; then
@@ -235,10 +295,14 @@ function invoke {
   fi
 
   if [ "$loop" = "external" ]; then
-     loop=true
      interval=$1
      count=$2
      shift 2
+  elif [ "$loop" = "options" ]; then
+     interval=$1
+     count=$2
+     shift 2
+     loop_options=$(eval echo $(cat $toolExecDir/$cmd.properties | grep loop | cut -d':' -f3))
   fi
 
   #hostname
@@ -249,8 +313,12 @@ function invoke {
   echo $CSVheader
 
   #run the tool
-  if [ "$loop" = "true" ]; then
+  if [ "$loop" = "external" ]; then
     $toolsBin/timedExec.sh $interval $count $uosmcDEBUG $toolExecDir/$cmd $@ \
+    | perl -ne "$perlBUFFER; print \"$hostname,$cmd,\$_\";" \
+    | $toolsBin/addTimestamp.pl $addTimestampBUFFER -timedelimiter=" " -delimiter=$CSVdelimiter
+  elif [ "$loop" = "options" ]; then
+    $toolExecDir/$cmd $loop_options $@ \
     | perl -ne "$perlBUFFER; print \"$hostname,$cmd,\$_\";" \
     | $toolsBin/addTimestamp.pl $addTimestampBUFFER -timedelimiter=" " -delimiter=$CSVdelimiter
   else
@@ -260,133 +328,21 @@ function invoke {
   fi
 }
 
-function locateCompatibleVersions {
- tools=$1
-
- if [ -z $tools ]; then
-  tools="iostat vmstat ifconfig"
- fi
-
- rm -f "$umcRoot/tools/$system_type/$version_major/$version_minor/*.Success"
- rm -f "$umcRoot/tools/$system_type/$version_major/$version_minor/*.Failure"
- 
- toolCnt=0
- for toolCmd in $tools; do
-   locateCompatilbleExecDir $toolCmd
-   ((toolCnt ++))
- done
-
- echo
- echo Summary of compatible versions. 
- cat "$umcRoot/tools/$system_type/$version_major/$version_minor/*.Success" | sort | uniq -c | sort -n -r | egrep "^\s+$toolCnt"
-}
-
-function reportCompatibilityResult {
-  toolCmd=$1
-  result=$2
-  version=$3
-
-  #decode current system version 
-  decodeVersion
-
-  mkdir -p $umcRoot/tools/$system_type/$version_major/$version_minor
-
-  if [ -f $umcRoot/tools/$system_type/$version_major/$version_minor/$toolCmd.Failure ]; then
-    rm $umcRoot/tools/$system_type/$version_major/$version_minor/$toolCmd.Failure 
-  fi
-  echo $version >> $umcRoot/tools/$system_type/$version_major/$version_minor/$toolCmd.$result
-}
-
-function testCompatibility {
-  toolCmd=$1
-  toolExecDir=$2
-
-  echo -n Testing compatibility of $2 with $1 ...
-
-  #check if tool is installed on this platform
-  assertInvoke $1
-  if [ $? -eq 2 ]; then
-    return 2
-  fi
-
-  #check if directory is available
-  if [ ! -f $toolExecDir/$toolCmd ]; then
-    echo "Error! Reason: The tool not found in given directory."
-    return 3
-  fi
-
-  #test tool compatibility. The rule is that if header is the same, it's ok.
-  headerlines=$(cat $toolExecDir/$toolCmd.rawheader | grep 'cfg:line' | cut -d':' -f3)
-  if [ ! -z $headerlines ]; then
-    systemHeader=$($toolCmd | sed -n "$headerlines"p)
-    thisHeader=$(cat $toolExecDir/$toolCmd.rawheader | grep -v 'cfg:')
-    if [ "$thisHeader" = "$systemHeader" ]; then
-      echo OK
-      reportCompatibilityResult $toolCmd Success $toolExecDir
-      return 0
-    fi
-  fi
-  headerscript=$(cat $toolExecDir/$toolCmd.rawheader | grep 'cfg:script' | cut -d':' -f3)
-  if [ ! -z "$headerscript" ]; then
-    systemHeader=$(. $toolExecDir/$headerscript)
-    thisHeader=$(cat $toolExecDir/$toolCmd.rawheader | grep -v 'cfg:')
-    if [ "$thisHeader" = "$systemHeader" ]; then
-      echo OK
-      reportCompatibilityResult $toolCmd Success $toolExecDir
-      return 0
-    fi
-  fi
-  headerCmd=$(cat $toolExecDir/$toolCmd.rawheader | grep 'cfg:command' | cut -d':' -f3-999)
-  if [ ! -z "$headerCmd" ]; then
-    systemHeader=$($toolExecDir/$headerCmd)
-    thisHeader=$(cat $toolExecDir/$toolCmd.rawheader | grep -v 'cfg:')
-    if [ "$thisHeader" = "$systemHeader" ]; then
-      echo OK
-      reportCompatibilityResult $toolCmd Success $toolExecDir
-      return 0
-    fi
-  fi
-
-  echo "Error! Reason: different header"
-  echo "Tested system header: $thisHeader"
-  echo "This system header  : $systemHeader"
-  reportCompatibilityResult $toolCmd Failure $toolExecDir
-  return 1
-}
-
-function locateCompatilbleExecDir {
-
-  toolCmd=$1
-
-
-  echo -n "Locating compatible wrappers for $1 ..."
-
-  #decode current system version 
-  decodeVersion
-
-  #decode current system version 
-  decodeVersion
-
-  if [ ! -d $umcRoot/tools/$system_type/$version_major ]; then
-    echo "Error! Reason: Not found any candidates for major version."
-    return 1
-  fi
-
-  echo ""
-  for version_minor_candidate in $(ls $umcRoot/tools/$system_type/$version_major)
-  do
-    echo -n "  - "
-    testCompatibility $toolCmd $umcRoot/tools/$system_type/$version_major/$version_minor_candidate
-  done
-}
 
 #---
 #--- Test Run
 #---
-function testRun {
- invoke iostat 1 3
- invoke vmstat 1 3
- invoke ifconfig 1 3
+function umcTestRun { 
+ for cmd in $(availableSensors); do
+    echo -n $cmd: 
+    invoke $cmd 1 1 >/dev/null
+    if [ $? -eq 0 ]; then
+        echo OK
+    else
+        echo Error
+        locateToolExecDir $cmd
+    fi
+ done
 }
 
 echo Universal Metrics Collector initialized.
--- etc/umcConfig.h
+++ etc/umcConfig.h
@@ -2,6 +2,7 @@
 #--- platform location & specific configuration
 #---------------------------------------------------------------------------------------
 
+#TODO configure below variables to used Oracle SOA data collectors
 export FMW_MOME=/oracle/fmwhome
 export WLS_HOME=$FMW_MOME/wlserver_10.3/server
 export SOA_HOME=$FMW_MOME/Oracle_SOA1
@@ -11,7 +12,11 @@ export DOMAIN_HOME=/oracle/fmwhome/user_projects/domains/dev_soasuite
 #--- platform elements 
 #---------------------------------------------------------------------------------------
 
-. $WLS_HOME/bin/setWLSEnv.sh
+if [ ! -z $WLS_HOME ]; then
+  . $WLS_HOME/bin/setWLSEnv.sh
+else
+  echo "Note: Oracle SOA not configured. Update umcConfig.h to be able to use SOA related components of the package."
+fi
 
 #---------------------------------------------------------------------------------------
 #--- reporting
@@ -40,34 +45,27 @@ export java_version_minor=$(echo $java_version | cut -f1 -d'_' | cut -d'.' -f2)
 export java_version_patch=$(echo $java_version | cut -f1 -d'_' | cut -d'.' -f3)
 export java_version_specific=$(echo $java_version | cut -f2 -d'_')
     
-#10.3.6.0
-export wls_version=$(cat $FMW_MOME/registry.xml | grep 'component name="WebLogic Server"' | tr ' ' '\n' | grep version | cut -d'=' -f2 | tr -d '"')
-export wls_version_major=$(echo $wls_version | cut -d'.' -f1)
-export wls_version_minor=$(echo $wls_version | cut -d'.' -f2)
-export wls_version_patch=$(echo $wls_version | cut -d'.' -f3)
-export wls_version_specific=$(echo $wls_version | cut -d'.' -f4)
-    
-#11.1.1.7.0
-export soa_version=$($SOA_HOME/bin/soaversion.sh | grep "Oracle SOA Server version" | cut -d' ' -f5)
-export soa_version_major=$(echo $soa_version | cut -d'.' -f1)
-export soa_version_minor=$(echo $soa_version | cut -d'.' -f2)
-export soa_version_patch=$(echo $soa_version | cut -d'.' -f3)
-export soa_version_specific=$(echo $soa_version | cut -d'.' -f4-5)
-    
+if [ ! -z $WLS_HOME ]; then
+    #10.3.6.0
+    export wls_version=$(cat $FMW_MOME/registry.xml | grep 'component name="WebLogic Server"' | tr ' ' '\n' | grep version | cut -d'=' -f2 | tr -d '"')
+    export wls_version_major=$(echo $wls_version | cut -d'.' -f1)
+    export wls_version_minor=$(echo $wls_version | cut -d'.' -f2)
+    export wls_version_patch=$(echo $wls_version | cut -d'.' -f3)
+    export wls_version_specific=$(echo $wls_version | cut -d'.' -f4)
+
+    #11.1.1.7.0
+    export soa_version=$($SOA_HOME/bin/soaversion.sh | grep "Oracle SOA Server version" | cut -d' ' -f5)
+    export soa_version_major=$(echo $soa_version | cut -d'.' -f1)
+    export soa_version_minor=$(echo $soa_version | cut -d'.' -f2)
+    export soa_version_patch=$(echo $soa_version | cut -d'.' -f3)
+    export soa_version_specific=$(echo $soa_version | cut -d'.' -f4-5)
+fi
+
 #---------------------------------------------------------------------------------------
 #--- tools category
 #---------------------------------------------------------------------------------------
 
-
-export platformLayers="hardware/linux/java/wls/soa"
-    
 export system_type=$(uname -s | tr [A-Z] [a-z])
-
-export linux_layer=hardware
-export java_layer=linux
-export wls_layer=java
-export soa_layer=wls    
-export soabindings_layer=soa
 
 export vmstat_layer=$system_type
 export free_layer=$system_type
@@ -75,12 +73,13 @@ export top_layer=$system_type
 export uptime_layer=$system_type
 export meminfo_layer=$system_type
 export tcpext_layer=$system_type
-export netstat_tcp_layer=$system_type
+export netstattcp_layer=$system_type
 export ifconfig_layer=$system_type
 export iostat_layer=$system_type
-    
+export soabindings_layer=soa
+
 #---------------------------------------------------------------------------------------
-#--- tools version
+#--- tools packages
 #---------------------------------------------------------------------------------------
 
 # use in code: cmd_version=$(eval "echo $(echo \$$cmd\_version)"); echo $cmd_version
@@ -96,7 +95,7 @@ export meminfo_package=procps/$(uptime -V  2>&1 | head -1 | sed 's/[^0-9.]*//g')
 export tcpext_package=procps/$(uptime -V  2>&1 | head -1 | sed 's/[^0-9.]*//g')
 
 # net-tools
-export netstat_tcp_package=net-tools/$(netstat -V  2>&1 | head -1 | sed 's/[^0-9.]*//g')
+export netstattcp_package=net-tools/$(netstat -V  2>&1 | head -1 | sed 's/[^0-9.]*//g')
 export ifconfig_package=net-tools/$(ifconfig -V  2>&1 | head -1 | sed 's/[^0-9.]*//g')
 
 # sysstat
