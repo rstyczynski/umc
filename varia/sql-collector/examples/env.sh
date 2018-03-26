d762a6e353a20e406672b58d409cd3605ee46231
tomvit <tomas@vitvar.com>
2018-03-26 18:33:38 +0200

    removed sql-collector dir

--- varia/sql-collector/examples/env.sh
+++
@@ -1,2 +0,0 @@
-# set db connection string here for examples to work
-#export DB_CONNSTR=""
--- varia/sql-collector/examples/sys-active-sessions.sql
+++
@@ -1,4 +0,0 @@
--- SQL queries running in the system
-SELECT a.sid, a.serial#, b.sql_text
-FROM v$session a, v$sqlarea b
-WHERE a.sql_address=b.address
