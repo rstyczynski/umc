d6f6834395fea0bc7989e7c3983f8056056d9269
rstyczynski <rstyczynski@dhcp-10-175-204-121.vpn.oracle.com>
2018-04-20 15:46:50 +0200

    shh key distribution check

---
+++ bin/ssh-check-key.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+umcRoot="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+port=22
+if [ "$1" == "-p" ]; then
+  port=$2
+  shift;shift
+fi
+servers=$1
+user=$(whoami)
+
+serversError=''
+for server in $servers; do
+  echo -n $server...
+  ssh -q -oBatchMode=yes -p $port $user@$server "echo OK" 2 >/dev/null
+  if [ $? -ne 0 ]; then
+    echo "Error"
+    serversError="$serversError $server"
+  fi
+done
+
+if [ -z "$serversError" ]; then
+    echo All good.
+else
+    echo "Servers with no key authentication:"
+    echo $serversError
+    echo
+    echo "Distribute keys using command:""
+    echo ssh-check-key.sh -p $port "$serversError"   
+fi
