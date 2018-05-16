4cb2a94f3daf3cf09721be25a1e538f097b30456
tomvit <tomas@vitvar.com>
2018-05-16 13:19:13 +0200

    oel 511 vm provisioning use external libraries installation script

--- test/oel-511/jdk-8u162-env.sh
+++
@@ -1,3 +0,0 @@
-export PATH=$HOME/libs/jdk1.8.0_162/bin/:$PATH
-export JAVA_HOME=$HOME/libs/jdk1.8.0_162
-
--- test/oel-511/python-2.7.11-env.sh
+++
@@ -1,2 +0,0 @@
-export PATH=$HOME/libs/python/Python-2.7.11/:$PATH
-export PYTHONPATH=$HOME/libs/python/Python-2.7.11
