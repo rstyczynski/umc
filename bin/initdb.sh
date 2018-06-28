de4efa10c0291d7e22a891af7b06b7f3b32f4ea3
tomvit <tomas@vitvar.com>
2018-06-29 01:06:57 +0200

    influxdb init script

---
+++ bin/initdb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# use this script as an example to initialize your influxdb DB
+DBNAME="rodmon"
+USER="rodmon"
+PASS="rodmon"
+
+run-influxd.sh 2>&1 >/dev/null
+
+# create user and the password
+# this will only work when the influxdb was created and requires no authentication
+influx -execute "CREATE USER $USER WITH PASSWORD '$PASS' WITH ALL PRIVILEGES"
+influx -execute "CREATE DATABASE $DBNAME WITH DURATION INF REPLICATION 1 SHARD DURATION 7d"
+
+# change the authentication
+pwd=$(pwd)
+cd $(dirname $(which influx))/../../etc/influxdb/
+rm *.bckp
+sed -i.bckp s/^.*auth-enabled.*=.*false/auth-enabled=true/g influxdb.conf
+cd $pwd
+
+# restart the db
+stop-influxd.sh 2>&1 >/dev/null
+run-influxd.sh 2>&1 >/dev/null
