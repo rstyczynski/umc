6b03fe7642fd904483c224b925a47aebdeab72d9
tomvit <tomas@vitvar.com>
2018-09-05 15:19:59 +0200

    timezone in sqlcl

--- varia/bin/install-libs.sh
+++ varia/bin/install-libs.sh
@@ -152,7 +152,7 @@ install_sqlcl () {
 		cd $LIBS_HOME && unzip $INSTALL_HOME/$BINFILE >>$INSTALL_LOG 2>&1
 		
 		# add min and max JVM memory
-		sed -i.bckp '/AddVMOption -Xss10M/a AddVMOption -Xms64m\nAddVMOption -Xmx64m' $LIBS_HOME/sqlcl/bin/sql
+		sed -i.bckp '/AddVMOption -Xss10M/a AddVMOption -Xms64m\nAddVMOption -Xmx64m\nAddVMOption -Duser.timezone=UTC' $LIBS_HOME/sqlcl/bin/sql
 		
 		# env variables
 		echo "# sqlcl" >>$ENV_FILE
