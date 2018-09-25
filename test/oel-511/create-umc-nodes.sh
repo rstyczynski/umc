ea2b9667978ec94e01dab38bf03f3973d5deb8fa
tomvit <tomas@vitvar.com>
2018-09-25 15:08:38 +0200

    docker influxdb nodes, fixes

--- test/oel-511/create-umc-nodes.sh
+++ test/oel-511/create-umc-nodes.sh
@@ -5,7 +5,7 @@
 # the script directory
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 UMC_HOME=$(cd $scriptDir && cd ../../../umc && pwd -P)   
-IMAGE="tomvit/umc-oel511:1.5"
+IMAGE="tomvit/umc-oel511:1.7"
 
 # create umc network
 docker network ls | grep umcnet >/dev/null
@@ -21,17 +21,33 @@ create_node() {
   nostart=$3
 
   echo "* creating node umc-$name, ip=$ip..."  
-  docker kill umc-$name &>/dev/null
-  docker rm umc-$name &>/dev/null
+  docker kill umc-$name >/dev/null
+  docker rm umc-$name >/dev/null
+
+  docker run -it \
+	--user oracle \
+	--net umcnet \
+	-h $name.umc.local \
+	-v $UMC_HOME:/home/oracle/umc \
+	--name umc-$name \
+	--ip $ip -d $IMAGE /bin/bash -l >/dev/null
+
   if [ "$nostart" != "nostart" ]; then
-    docker run --user oracle --net umcnet -it -h $name -v $UMC_HOME:/home/oracle/umc --name umc-$name --ip $ip -d $IMAGE \
-	/bin/bash -l -c 'umcrunnerd --verbose &>/home/oracle/umc/bin/$HOSTNAME.out' 
-  else
-    docker run --user oracle --net umcnet -it -h $name -v $UMC_HOME:/home/oracle/umc --name umc-$name --ip $ip -d $IMAGE /bin/bash -l
+    docker exec umc-$1 /bin/bash -l -c "start-umcrunner --verbose"
   fi
 }
 
+create_idb_node() {
+  create_node $1 $2 nostart
+  docker exec umc-$1 /bin/bash -l -c "start-influxd.sh"
+  docker exec umc-$1 /bin/bash -l -c "influx -execute \"CREATE DATABASE rodmon_sample WITH DURATION INF REPLICATION 1 SHARD DURATION 7d\""
+  docker exec umc-$1 /bin/bash -l -c "start-idbpush --verbose"
+}
+
+# create umcrunnerd nodes
 create_node ukbn01hr 192.168.10.101 $1
 create_node ukbn02hr 192.168.10.102 $1
 create_node ukbn03hr 192.168.10.103 $1
 
+# create influxdb and idbpush node
+create_idb_node ukbn10hr 192.168.10.110 nostart
