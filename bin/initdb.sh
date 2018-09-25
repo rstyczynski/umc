461e4fb3100928b779a909be7e6e637f15d750ce
tomvit <tomas@vitvar.com>
2018-09-25 15:07:42 +0200

    helper start/stop scripts change

--- bin/initdb.sh
+++ bin/initdb.sh
@@ -3,34 +3,34 @@
 # backup command: influxd backup -database rodmon_vfuk_npp tmp/
 
 # variables
-DBNAME="rodmon_vfuk_npp"
-USER="rodmon"
-PASS="rodmon"
-
-echo "* starting db..."
-start-influxd.sh
-
-sleep 1
-
-echo "* creating user '$USER'"
-influx -execute "CREATE USER $USER WITH PASSWORD '$PASS' WITH ALL PRIVILEGES"
-
-echo "* stopping db..."
-stop-influxd.sh 
-
-echo "* changing the authentication"
-# change the authentication
-pwd=$(pwd)
-cd $(dirname $(which influx))/../../etc/influxdb/
-rm -f *.bckp
-sed -i.bckp s/^.*auth-enabled.*=.*false/auth-enabled=true/g influxdb.conf
-cd $pwd
-
-echo "* starting db..."
-start-influxd.sh
-
-sleep 1
-
-echo "* creating db '$DBNAME'"
-influx --username '$USER' --password '$PASS' -execute "CREATE DATABASE $DBNAME WITH DURATION INF REPLICATION 1 SHARD DURATION 7d"
+# DBNAME="rodmon_vfuk_npp"
+# USER="rodmon"
+# PASS="rodmon"
+# 
+# echo "* starting db..."
+# start-influxd.sh
+# 
+# sleep 1
+# 
+# echo "* creating user '$USER'"
+# influx -execute "CREATE USER $USER WITH PASSWORD '$PASS' WITH ALL PRIVILEGES"
+# 
+# echo "* stopping db..."
+# stop-influxd.sh 
+# 
+# echo "* changing the authentication"
+# # change the authentication
+# pwd=$(pwd)
+# cd $(dirname $(which influx))/../../etc/influxdb/
+# rm -f *.bckp
+# sed -i.bckp s/^.*auth-enabled.*=.*false/auth-enabled=true/g influxdb.conf
+# cd $pwd
+# 
+# echo "* starting db..."
+# start-influxd.sh
+# 
+# sleep 1
+# 
+# echo "* creating db '$DBNAME'"
+# influx --username '$USER' --password '$PASS' -execute "CREATE DATABASE $DBNAME WITH DURATION INF REPLICATION 1 SHARD DURATION 7d"
 
