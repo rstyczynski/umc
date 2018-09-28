72fb5dfff76bef69fcdc1953d3bbe7d0a1213eaa
tomvit <tomas@vitvar.com>
2018-09-28 23:46:58 +0200

    tail function

--- bin/libs/utils.py
+++ bin/libs/utils.py
@@ -151,3 +151,9 @@ def fd_table_status_str():
     fd_result = fd_table_status()
     return ', '.join(['{0}: {1}'.format(*i) for i in fd_result])
     
+def tail(f, n, offset=0):
+    stdin,stdout = os.popen2("tail -n "+str(n)+" "+f)
+    stdin.close()
+    lines = stdout.readlines(); stdout.close()
+    return lines    
+
