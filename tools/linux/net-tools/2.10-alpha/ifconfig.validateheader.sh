6f82c991995f0abe29e31771a62e7e30c316c8a9
rstyczynski <rstyczynski@gmail.com>
2020-02-11 22:16:42 +0300

    EL7 net-tools 2.10-alpha

---
+++ tools/linux/net-tools/2.10-alpha/ifconfig.validateheader.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+ifname=$(ifconfig | head -1 | cut -f1 -d':')
+
+ifconfig $ifname | grep X | 
+sed 's/RX packets /RX packets:/g'| 
+sed 's/TX packets /TX packets:/g'| 
+sed 's/RX errors /RX errors:/g'| 
+sed 's/TX errors /TX errors:/g'| 
+sed 's/dropped /dropped:/g'| 
+sed 's/overruns /overruns:/g'| 
+sed 's/frame / frame:/g'| 
+sed 's/carrier /carrier:/g'| 
+sed 's/collisions /collisions:/g'| 
+sed 's/[0-9]//g' | sed 's/(. \w*B)//g' | tr -d ' ' | tr -d '\n'
