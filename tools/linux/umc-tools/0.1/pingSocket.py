34d87dbee0671232d96b1c707f5999ccb788d387
rstyczynski <rstyczynski@gmail.com>
2020-07-27 11:52:21 +0200

    .

--- tools/linux/umc-tools/0.1/pingSocket.py
+++ tools/linux/umc-tools/0.1/pingSocket.py
@@ -79,7 +79,7 @@ def getStats(targetSystem, targetPort):
         # UDP
         if (monitor_transport == socket.SOCK_DGRAM):
             start_time = time.time()
-            socket.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+            socket.sendto('Hello!', (addressStr, targetPort))
             ms = (time.time() - start_time ) * 1000
             connect=ms
 
