d837a29f6ed29c4688e0a3da638063745184d09a
rstyczynski <rstyczynski@gmail.com>
2020-08-06 21:53:20 +0200

    .

--- lib/net-service.sh
+++ lib/net-service.sh
@@ -92,7 +92,7 @@ function start() {
             echo $! >>$umc_run/$svc_name.pid
 
             (
-                umc mtr collect 5 288 $address |
+                umc mtr collect 1 1440 $address |
                     $umc_bin/csv2obd --resource mtr_$service_name-$target_name |
                     $umc_bin/logdirector.pl -dir /var/log/umc -addDateSubDir -name mtr_$service_name-$target_name -detectHeader -checkHeaderDups -flush
             ) &
