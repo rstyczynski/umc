9e15f19617759cd04844ab18a4f8d6cf2a57ec37
rstyczynski <rstyczynski@gmail.com>
2020-08-27 09:06:07 +0200

    .

--- varia/trace/deploy_trace.sh
+++ varia/trace/deploy_trace.sh
@@ -1,39 +1,36 @@
 #!/bin/bash
 
-server_env=$1
-server_type=$2
-shared_trace=$3
-
-if [ -z "$shared_trace" ] || [ -z "$server_env" ] || [ -z "$server_type" ]; then
-    echo "Usage: config_trace.sh server_env server_type shared_trace"
+if [ -z "$shared_trace_root" ] || [ -z "$server_env" ] || [ -z "$server_type" ]; then
+    echo "Usage: config_trace.sh 
+, with exported veriables: server_env, server_type, shared_trace_root"
     return 1
 fi
 
-if [ ! -d $shared_trace ]; then
-    echo "Error! Shared directory does not exist: $shared_trace"
+if [ ! -d $shared_trace_root ]; then
+    echo "Error! Shared directory does not exist: $shared_trace_root"
     return 2
 fi
 
-if [ ! -d $shared_trace/cfg/$server_env/$server_type ]; then
-    echo "Error! Server directory does not exist: $shared_trace/cfg/$server_env/$server_type"
+if [ ! -d $shared_trace_root/cfg/$server_env/$server_type ]; then
+    echo "Error! Server directory does not exist: $shared_trace_root/cfg/$server_env/$server_type"
     return 3
 fi
 
-if [ $(ls $shared_trace/cfg/$server_env/$server_type/os-probe_$server_env\-$server_type.yml | wc -l) -eq 0 ]; then
-    echo "Error! OS resource definition deas not exist at $shared_trace/cfg/$server_env/$server_type/os-probe_$server_env\-$server_type.yml"
+if [ $(ls $shared_trace_root/cfg/$server_env/$server_type/os-probe_$server_env\-$server_type.yml | wc -l) -eq 0 ]; then
+    echo "Error! OS resource definition deas not exist at $shared_trace_root/cfg/$server_env/$server_type/os-probe_$server_env\-$server_type.yml"
     return 4
 fi
 
-if [ $(ls $shared_trace/cfg/$server_env/ext/net-probe_*.yml | wc -l) -eq 0 ]; then
-    echo "Error! Network resource definition deas not exist at $shared_trace/cfg/$server_env/ext/net-probe_*.yml"
+if [ $(ls $shared_trace_root/cfg/$server_env/ext/net-probe_*.yml | wc -l) -eq 0 ]; then
+    echo "Error! Network resource definition deas not exist at $shared_trace_root/cfg/$server_env/ext/net-probe_*.yml"
     return 4
 fi
 
 #
 echo copy resource definitions...
 #
-cp $shared_trace/cfg/$server_env/$server_type/os-probe_$server_env\-$server_type.yml ~/.umc
-cp $shared_trace/cfg/$server_env/ext/net-probe_*.yml ~/.umc
+cp $shared_trace_root/cfg/$server_env/$server_type/os-probe_$server_env\-$server_type.yml ~/.umc
+cp $shared_trace_root/cfg/$server_env/ext/net-probe_*.yml ~/.umc
 
 #
 echo update bash_profile
@@ -54,8 +51,8 @@ export umc_log=~/trace/log
 mkdir -p ~/.umc
 mkdir -p $status_root
 mkdir -p $umc_log
-mkdir -p $shared_trace/$server_env/$(hostname)/runtime/obd
-mkdir -p $shared_trace/$server_env/$(hostname)/runtime/log
+mkdir -p $shared_trace_root/$server_env/$(hostname)/runtime/obd
+mkdir -p $shared_trace_root/$server_env/$(hostname)/runtime/log
 
 # umc envs. - STOP
 EOF
@@ -72,8 +69,8 @@ echo "1 0 * * * ~/umc/lib/os-service.sh $os_service restart" >> cron.tmp
 for net_service in $(cd ~/.umc; ls net-probe_*.yml); do 
   echo "1 0 * * * ~/umc/lib/net-service.sh $net_service restart" >> cron.tmp
 done
-echo "* * * * * rsync -rt ~/trace/obd/ \$shared_trace/\$server_env/\$(hostname)/runtime/obd" >>  cron.tmp
-echo "* * * * * rsync -rt ~/trace/log/ \$shared_trace/\$server_env/\$(hostname)/runtime/log" >>  cron.tmp
+echo "* * * * * rsync -rt ~/trace/obd/ \$shared_trace_root/\$server_env/\$(hostname)/runtime/obd" >>  cron.tmp
+echo "* * * * * rsync -rt ~/trace/log/ \$shared_trace_root/\$server_env/\$(hostname)/runtime/log" >>  cron.tmp
 echo "# stop - umc" >>  cron.tmp
 
 (crontab -l 2>/dev/null |
@@ -98,7 +95,7 @@ done
 echo test log and state data...
 #
 ll ~/trace/log
-ll $shared_trace/$server_env/$(hostname)/runtime/log
+ll $shared_trace_root/$server_env/$(hostname)/runtime/log
 
 ll ~/trace/obd
-ll $shared_trace/$server_env/$(hostname)/runtime/obd
+ll $shared_trace_root/$server_env/$(hostname)/runtime/obd
