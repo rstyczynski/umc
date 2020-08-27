0c0d20412e7547d43e05a0140d30d8c26c8e623d
rstyczynski <rstyczynski@gmail.com>
2020-08-27 09:25:42 +0200

    .

--- varia/trace/deploy_trace.sh
+++ varia/trace/deploy_trace.sh
@@ -26,15 +26,17 @@ if [ $(ls $shared_trace_root/cfg/$server_env/ext/net-probe_*.yml | wc -l) -eq 0 
     return 4
 fi
 
-#
-echo copy resource definitions...
-#
+echo "#"
+echo "# copy resource definitions"
+echo "#"
+
 cp $shared_trace_root/cfg/$server_env/$server_type/os-probe_$server_env\-$server_type.yml ~/.umc
 cp $shared_trace_root/cfg/$server_env/ext/net-probe_*.yml ~/.umc
 
-#
-echo update bash_profile
-#
+echo "#"
+echo "# update bash_profile"
+echo "#"
+
 cp ~/.bash_profile ~/.bash_profile.bak
 cat ~/.bash_profile.bak | sed '/# umc envs. - START/,/# umc envs. - STOP/d'  > ~/.bash_profile
 
@@ -59,9 +61,10 @@ EOF
 cat ~/.bash_profile
 source ~/.bash_profile
 
-#
-echo update cron...
-#
+echo "#"
+echo "# update cron"
+echo "#"
+
 echo "# start - umc" > cron.tmp
 os_service=os-probe_$server_env\-$server_type.yml
 echo "1 0 * * * ~/umc/lib/os-service.sh $os_service restart" >> cron.tmp
@@ -70,16 +73,17 @@ for net_service in $(cd ~/.umc; ls net-probe_*.yml); do
   echo "1 0 * * * ~/umc/lib/net-service.sh $net_service restart" >> cron.tmp
 done
 echo "* * * * * rsync -rt ~/trace/obd/ \$shared_trace_root/\$server_env/\$(hostname)/runtime/obd" >>  cron.tmp
-echo "* * * * * rsync -rt ~/trace/log/ \$shared_trace_root/\$server_env/\$(hostname)/runtime/log" >>  cron.tmp
+echo "* * * * * rsync -t ~/trace/log/ \$shared_trace_root/\$server_env/\$(hostname)/runtime/log/\$(date +%Y-%m-%d)" >>  cron.tmp
 echo "# stop - umc" >>  cron.tmp
 
 (crontab -l 2>/dev/null |
 sed '/# start - umc/,/# stop - umc/d'
 cat cron.tmp) | crontab -; rm cron.tmp; crontab -l
 
-#
-echo start umc metric collection services...
-#
+echo "#"
+echo "# start umc metric collection services"
+echo "#"
+echo ...
 source ~/.bash_profile
 
 os_service=os-probe_$server_env\-$server_type.yml
@@ -91,11 +95,21 @@ for net_service in $(cd ~/.umc; ls net-probe_*.yml); do
   nohup ~/umc/lib/net-service.sh $net_service start block > $net_service.log &
 done
 
-#
-echo test log and state data...
-#
+rsync -rt ~/trace/obd/ $shared_trace/$server_env/$(hostname)/runtime/obd
+rsync -t ~/trace/log/ $shared_trace/$server_env/$(hostname)/runtime/log/$(date +%Y-%m-%d)
+
+
+echo "#"
+echo "# look at logs"
+echo "#"
 ll ~/trace/log
 ll $shared_trace_root/$server_env/$(hostname)/runtime/log
+ls $shared_trace_root/$server_env/$(hostname)/runtime/log/$(date +%Y-%m-%d)
 
+echo "#"
+echo "# look at state data"
+echo "#"
 ll ~/trace/obd
+cat ~/trace/obd/system-vmstat/state
 ll $shared_trace_root/$server_env/$(hostname)/runtime/obd
+cat $shared_trace_root/$server_env/$(hostname)/runtime/obd/
