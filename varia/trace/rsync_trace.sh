3ba7d83dbccf8e3c6435f52740e795063bd0be8c
rstyczynski <rstyczynski@gmail.com>
2020-08-28 20:50:45 +0200

    rstyn params

--- varia/trace/rsync_trace.sh
+++ varia/trace/rsync_trace.sh
@@ -4,15 +4,18 @@
 : ${server_type:=$2}
 : ${shared_trace_root:=$3}
 
+: ${umc_log:=~/trace/log}
+: ${status_root:=~/trace/obd}
+
 if [ -z "$shared_trace_root" ] || [ -z "$server_env" ] || [ -z "$server_type" ]; then
     echo "Usage: rsync_trace.sh 
 , with exported veriables: server_env, server_type, shared_trace_root"
     exit 1
 fi
 
-rsync -rt ~/trace/obd/ $shared_trace_root/$server_env/$(hostname)/runtime/obd
+rsync -rt $status_root/ $shared_trace_root/$server_env/$(hostname)/runtime/obd
 
 if [ ! -d $shared_trace_root/$server_env/$(hostname)/runtime/log/$(date +%Y-%m-%d) ]; then
     mkdir -p $shared_trace_root/$server_env/$(hostname)/runtime/log/$(date +%Y-%m-%d)
 fi
-rsync -t ~/trace/log/$(date +%Y-%m-%d)/* $shared_trace_root/$server_env/$(hostname)/runtime/log/$(date +%Y-%m-%d)
+rsync -t $umc_log/$(date +%Y-%m-%d)/* $shared_trace_root/$server_env/$(hostname)/runtime/log/$(date +%Y-%m-%d)
