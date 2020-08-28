4ea7964e65117f3473bc20f5b90b87d9fef0e799
rstyczynski <rstyczynski@gmail.com>
2020-08-28 20:44:17 +0200

    rsync default values

--- varia/trace/rsync_trace.sh
+++ varia/trace/rsync_trace.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-server_env=$1
-server_type=$2
-shared_trace_root=$3
+: ${server_env:=$1}
+: ${server_type:=$2}
+: ${shared_trace_root:=$3}
 
 if [ -z "$shared_trace_root" ] || [ -z "$server_env" ] || [ -z "$server_type" ]; then
     echo "Usage: rsync_trace.sh 
