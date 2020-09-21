5c56a53e3f9a80652f2609aa03b04e76e37f4c3e
rstyczynski <rstyczynski@gmail.com>
2020-09-21 14:55:23 +0200

    http_proxy control for dms

--- lib/soadms-service.sh
+++ lib/soadms-service.sh
@@ -131,8 +131,26 @@ if [ "$operation" == "reset-dms" ]; then
         exit 1
     fi
 
+    #
+    # control http proxy
+    # 
     url=$(cat $umc_cfg/$umc_svc_def | y2j | jq -r '.soadms.url')
- 
+    http_proxy=$(cat $umc_cfg/$umc_svc_def | y2j | jq -r '.soadms.http_proxy')
+    if [ ! -z "$http_proxy" ] && [ "$http_proxy" != null ]; then
+        export http_proxy=$http_proxy
+    fi
+    if ["$http_proxy" == "unset" ]; then
+        unset http_proxy
+    fi
+
+    https_proxy=$(cat $umc_cfg/$umc_svc_def | y2j | jq -r '.soadms.https_proxy')
+    if [ ! -z "$https_proxy" ] && [ "$https_proxy" != null ]; then
+        export https_proxy=$https_proxy
+    fi
+    if ["$https_proxy" == "unset" ]; then
+        unset https_proxy
+    fi
+
     #
     # get user/pass
     #
