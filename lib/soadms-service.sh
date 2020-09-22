14748dd0da7751de64b7ca707d0050b487b4a377
rstyczynski <rstyczynski@gmail.com>
2020-09-22 16:58:08 +0200

    .

--- lib/soadms-service.sh
+++ lib/soadms-service.sh
@@ -280,11 +280,11 @@ function start() {
         fi
 
         echo "> collector:$dms_table, interval: $interval"
-        (
-            export dms_reset
-            export dms_reset_path
-            export umc_svc_def
 
+        export dms_reset
+        export dms_reset_path
+        export umc_svc_def
+        (
             umc soadms collect $interval $count --table $dms_table --url $url --connect $user/$pass |
             $umc_bin/logdirector.pl -dir $umc_log -addDateSubDir -name soadms_$dms_table -detectHeader -checkHeaderDups -tee |
             $umc_bin/csv2obd --resource $resource_id --resource_log_prefix $umc_log/$(date +%Y-%m-%d)/$resource_log_prefix >/dev/null
